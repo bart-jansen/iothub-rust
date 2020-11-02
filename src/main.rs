31c1b493b10c678344e508f2e369ddbe97c4b145
Bart Jansen <bajansen@microsoft.com>
2020-11-02 14:49:28 +0000

    added yahoo api

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io::Write;
 use std::net::TcpStream;
 use std::fs::File;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, UNIX_EPOCH};
 use mqtt::{Encodable, Decodable};
 use mqtt::packet::*;
 use mqtt::{TopicName};
@@ -15,6 +15,9 @@ use mqtt::control::variable_header::ConnectReturnCode;
 use clap::{Arg, App};
 use config::read_config;
 
+
+use yahoo_finance_api as yahoo;
+use chrono::prelude::*;
 
 fn connect(broker: String,
            username: String,
@@ -56,35 +59,56 @@ fn main() {
         .version("0.2.0")
         .author("Claus Matzinger. <claus.matzinger+kb@gmail.com>")
         .about("Sends data to an MQTT broker")
-        .arg(Arg::with_name("config")
+        .arg(Arg::with_name("ticker_symbol")
                  .short("c")
                  .long("config")
                  .help("Sets a custom config file [default: config.toml]")
-                 .value_name("config.toml")
+                 .default_value("BTC-USD")
                  .takes_value(true))
         .get_matches();
-    let config_filename = matches.value_of("config").unwrap_or("config.toml");
-    let mut f = File::open(config_filename)
-        .expect(&format!("Can't open configuration file: {}", config_filename));
-    let settings = read_config(&mut f).expect("Can't read configuration file.");
-    println!("Connecting to mqtts://{}", settings.mqtt.broker_address);
 
-    let topic_name = TopicName::new(settings.mqtt.topic.clone()).unwrap();
+    let ticker_symbol = matches.value_of("ticker_symbol");
+    // let mut f = File::open(config_filename)
+    //     .expect(&format!("Can't open configuration file: {}", config_filename));
+    // let settings = read_config(&mut f).expect("Can't read configuration file.");
+    // println!("Connecting to mqtts://{}", settings.mqtt.broker_address);
 
-    let mut stream = connect(settings.mqtt.broker_address,
-                             settings.mqtt.username,
-                             settings.mqtt.password,
-                             settings.mqtt.client_id,
-                             settings.mqtt.broker);
+    // let topic_name = TopicName::new(settings.mqtt.topic.clone()).unwrap();
+
+    // let mut stream = connect(settings.mqtt.broker_address,
+    //                          settings.mqtt.username,
+    //                          settings.mqtt.password,
+    //                          settings.mqtt.client_id,
+    //                          settings.mqtt.broker);
 
     let mut i = 0;
     loop {
         i += 1;
-        let msg = format!("{}", i);
-        println!("Sending message '{}' to topic: '{}'",
-                 msg,
-                 settings.mqtt.topic);
-        publish(&mut stream, msg, topic_name.clone());
+        // let msg = format!("{}", i);
+        // println!("Sending message '{}' to topic: '{}'",
+        //          msg,
+        //          settings.mqtt.topic);
+        // publish(&mut stream, msg, topic_name.clone());
+        
+        
+
+        let provider = yahoo::YahooConnector::new();
+    
+        if let Ok(response) = provider.get_latest_quotes(ticker_symbol.unwrap(), "1m") {
+            println!("timestamp {:?}", response.last_quote().unwrap().timestamp);
+            println!("price {:?}", response.last_quote().unwrap().open);
+
+        }
+
         thread::sleep(Duration::from_millis(3000));
+            
+        // get the latest quotes in 1 minute intervals
+        // let response = tokio_test::block_on(provider.get_latest_quotes("AAPL", "1m")).unwrap();
+        // extract just the latest valid quote summery
+        // including timestamp,open,close,high,low,volume
+        // let quote = response.last_quote().unwrap();
+        // let time: DateTime<Utc> =
+        //     DateTime::from(UNIX_EPOCH + Duration::from_secs(quote.timestamp));
+        // println!("At {} quote price of Apple was {}", time.to_rfc3339(), quote.close);
     }
 }
